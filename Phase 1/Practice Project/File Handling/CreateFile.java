081f22532fe80bb36be9425cd28132f31e23bef0
Saurabh Kumar Tiwari <48514521+saurabhtiwari28@users.noreply.github.com>
2022-03-09 02:15:57 +0530

    Add file via upload

---
+++ Phase 1/Practice Project/File Handling/CreateFile.java
@@ -0,0 +1,64 @@
+package com.practiceproject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
+
+public class CreateFile {
+	
+	public static void main(String[] args) throws IOException
+    {
+        createFileUsingFileClass();
+        createFileUsingFileOutputStreamClass();
+        createFileIn_NIO();
+    }
+ 
+    private static void createFileUsingFileClass() throws IOException
+    {
+          File file = new File("C:\\Users\\SAURABH\\Documents\\Hello\\HelloWorld.txt");
+  
+          
+          if (file.createNewFile()){
+            System.out.println("File is created!");
+          }else{
+            System.out.println("File already exists.");
+          }
+           
+          FileWriter writer = new FileWriter(file);
+          writer.write("Test data");
+          writer.close();
+    }
+ 
+    private static void createFileUsingFileOutputStreamClass() throws IOException
+    {
+        String data = "Test data";
+        FileOutputStream out = new FileOutputStream("C:\\Users\\SAURABH\\Documents\\Hello\\HelloWorld.txt");
+        out.write(data.getBytes());
+        out.close();
+    }
+ 
+ 
+
+
+   private static void createFileIn_NIO()  throws IOException
+    {
+        String data = "Test data";
+        Files.write(Paths.get("C:\\Users\\SAURABH\\Documents\\Hello\\HelloWorld.txt"), data.getBytes());
+        List<String> lines = Arrays.asList("1st line", "2nd line");
+       Files.write(Paths.get("HelloWorld.txt"),
+                    lines,
+                    StandardCharsets.UTF_8,
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.APPEND);
+    }
+}
+
+
+
