30eb6000cfdef77e981fe79c3d42b6839dfc014a
Saurabh Kumar Tiwari <48514521+saurabhtiwari28@users.noreply.github.com>
2022-03-12 23:16:01 +0530

    Add files via upload

---
+++ Phase 2/Product Details Portal/product.java
@@ -0,0 +1,42 @@
+package com.controller;
+
+public class product{
+	private int id;
+	private String pname;
+	private String ptype;
+	private int pprice;
+	
+	public product() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPname() {
+		return pname;
+	}
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+	public String getPtype() {
+		return ptype;
+	}
+
+	public void setPtype(String ptype) {
+		this.ptype = ptype;
+	}
+
+	public int getPprice() {
+		return pprice;
+	}
+
+	public void setPprice(int pprice) {
+		this.pprice = pprice;
+	}
+}
